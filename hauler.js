f9c221e07c987de0a87e2f9e47d39135eb97e364
Jack Peterson <jack@tinybike.net>
2015-11-22 04:58:07 -0800

    Fixes

--- hauler.js
+++ hauler.js
@@ -6,6 +6,8 @@ var net = require("net");
 var spacebox = require("./");
 
 var socket = net.connect({
+    // host: "sync.hauler.io",
+    // port: "80"
     host: "localhost",
     port: 9876
 }, function () {
@@ -14,18 +16,39 @@ var socket = net.connect({
 
 socket.on("data", function (data) {
     data = JSON.parse(data);
-    var handle = data.handle;
-    var files = data.payload;
-    spacebox.synchronize(files, function (err, updates) {
-        if (err) {
-            console.error(err);
-            return socket.end();
+    if (data && data.label) {
+        console.log(data);
+        switch (data.label) {
+        case "synchronize":
+            spacebox.synchronize(data.payload, function (err, updates) {
+                if (err) {
+                    console.error(err);
+                    return socket.end();
+                }
+                socket.write(JSON.stringify({
+                    label: "synchronized",
+                    handle: data.handle,
+                    payload: updates
+                }));
+            });
+            break;
+
+        case "upload":
+            spacebox.upload(data.path, data.options, function (err, files) {
+                if (err || !files) {
+                    return console.error("upload failed:", err, files);
+                }
+                socket.write(JSON.stringify({
+                    label: "uploaded",
+                    handle: data.handle,
+                    payload: files
+                }));
+            });
+            break;
+
+        default:
+            console.error("unknown label:", data.label);
+            console.log(JSON.stringify(data, null, 2));
         }
-        var response = {
-            label: "synchronized",
-            handle: handle,
-            payload: updates
-        };
-        socket.write(JSON.stringify(response));
-    });
+    }
 });
